cf89768be98ab6184d72dac9f05725ba3645a55d
Santosh Desiraju <desirajusantosh@gmail.com>
2015-08-08 19:08:12 -0700

    Update plotLine.py

--- plotLine.py
+++ plotLine.py
@@ -1,6 +1,9 @@
 import matplotlib as plt
 #X-axis sorted for making the graph clean from crazy lines all over and just a single line
 
+print(plt.style.available)
+
+plt.style.use('dark_background')
 forest_fires = forest_fires.sort(["rain"])
 plt.plot(forest_fires["rain"], forest_fires["area"])
 # Set the x axis label
