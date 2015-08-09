d4524a03855219a873b856aa14185af7dcb91b84
Santosh Desiraju <desirajusantosh@gmail.com>
2015-08-08 17:34:47 -0700

    Update plotScatter.py

--- plotScatter.py
+++ plotScatter.py
@@ -4,4 +4,7 @@ month = [1,1,2,3,4,4,6,6,7,7,8,11,11,12]
 temperature = [11,22,21,18,31,24,29,22,9,14,21,11,25]
 
 plt.scatter(month, temperature)
+plt.xlabel('month')
+plt.xlabel('temperature')
+plt.title('Temperature variation over the months')
 plt.show()
